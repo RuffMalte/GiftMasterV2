3649ae453a1db9402f463c40c99200d774d2b243
Malte Ruff <112979235+RuffMalte@users.noreply.github.com>
2023-11-04 22:08:57 +0100

    Initial Commit

---
+++ GiftMaster/GiftMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftMasterApp.swift
+//  GiftMaster
+//
+//  Created by Malte Ruff on 04.11.23.
+//
+
+import SwiftUI
+
+@main
+struct GiftMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
