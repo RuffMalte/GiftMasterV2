2280d2de87449dc090a47dd8b228479eba44d2a4
Malte Ruff <112979235+RuffMalte@users.noreply.github.com>
2023-11-19 15:39:12 +0100

    added Haptics

---
+++ GiftMaster/Haptics/Haptics.swift
@@ -0,0 +1,22 @@
+import Foundation
+import SwiftUI
+
+// private var selectedStyle: UIImpactFeedbackGenerator.FeedbackStyle = .light
+
+
+class Haptics {
+	
+	
+	func playFeedbackHaptic(_ style: UIImpactFeedbackGenerator.FeedbackStyle) {
+		let generator = UIImpactFeedbackGenerator(style: style)
+		generator.impactOccurred()
+	}
+	
+	
+	func playNotificationHaptic(_ type: UINotificationFeedbackGenerator.FeedbackType) {
+		let generator = UINotificationFeedbackGenerator()
+		generator.notificationOccurred(type)
+	}
+}
+
+
---
+++ GiftMaster/Views/HelperViews/DropViews/DropViews.swift
@@ -0,0 +1,18 @@
+//
+//  DropViews.swift
+//  GiftMaster
+//
+//  Created by Malte Ruff on 19.11.23.
+//
+
+import SwiftUI
+
+struct DropViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DropViews()
+}
